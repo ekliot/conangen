9c0b83bbceb04771e96823f72f9e502c8212523f
Elijah Kliot <ekliot@gmail.com>
2016-07-07 19:25:43 -0400

    touchup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,10 +5,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+      post signup_path, params: { user: { name:  "",
+                                          email: "user@invalid",
+                                          password:              "foo",
+                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
   end
