9ea5344cb0386eaaa998b0b472b03df1c993a374
Chris Lemelin <clemelin2@gmail.com>
2016-07-16 20:42:32 -0400

    Updated natures,eduaction and archtypes

--- app/models/lifepath/education.rb
+++ app/models/lifepath/education.rb
@@ -1,2 +1,14 @@
 class Lifepath::Education < ApplicationRecord
+  def get_talents
+    skills =  [ self.skill_mandatory1,  self.skill_mandatory2, self.skill_mandatory3,
+                self.skill_elective1,   self.skill_elective2,  self.skill_elective3 ]
+
+    talents = {}
+
+    skills.each do |skill|
+      talents[skill] = Talent.where( skill: skill ).find_each
+    end
+
+    talents
+  end
 end
