c0e56bd25a9653ddbecee37ff07a2965107200f3
ekliot <ekliot@gmail.com>
2017-01-05 15:53:31 -0500

    added d20 roller to char helper, and title generator, plus skeleton for title generator tests

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -13,6 +13,62 @@ module CharactersHelper
     :melee,       :observation,     :persuade,  :ranged,  :society
   ]
 
+  # generates a title/intro for a character given their lifepath
+  # can be formatted for HTML
+  # formatted as:
+  #   all three args present:
+  #       "Hither came $name, the $archetype, of $homeland..."
+  #   name is blank:
+  #       "Hither came a wanderer, a(n) $archetype, of $homeland..."
+  #   archetype is blank:
+  #       "Hither came $name, of $homeland..."
+  #   homeland is blank:
+  #       "Hither came $name, the $archetype..."
+  #   only name:
+  #       "Hither came $name..."
+  #   only archetype:
+  #       "Hither came a wanderer, a(n) $archetype..."
+  #   only homeland:
+  #       "Hither came a wanderer, of $homeland..."
+  #   none:
+  #       "Hither came a wanderer..."
+  def self.gen_title( name, archetype, homeland, html )
+    title =  html ? '<span id="CharTitle">' : ''
+
+    title << html ? '<span id="CharTitleIntro">Hither came</span>' : "Hither came"
+
+    if !name.blank?
+      title << html ? '<span id="CharTitleName"> #{name}</span>' : ' #{name}'
+    else
+      title << html ? '<span id="CharTitleName"> a wanderer</span>' : ' a wanderer'
+    end
+
+    if !archetype.blank?
+      article = ''
+
+      if name.blank?
+        article = %w(a e i o u).include?( archetype[0].downcase ) ? 'an' : 'a'
+      else
+        article = 'the'
+      end
+
+      title << html ? ', <span id="CharTitleArchetype"> #{article} #{archetype}</span>': ', #{article} #{archetype}'
+    end
+
+    if !homeland.blank?
+      title << html ? ', <span id="CharTitleHomeland"> of #{homeland}</span>' : ', of #{homeland}'
+    end
+
+    title << '...'
+
+    title
+  end
+
+  def self.d20( seed )
+    die = Random.new( seed )
+    die
+  end
+
   def self.skills
     @@skills
   end
