77026aa3e733ab8731761c716b2a588722bba980
ekliot <ekliot@gmail.com>
2017-02-06 09:29:30 -0500

    indent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
-  
+
   def new
-   @user = User.new
-   p 'new'
+    @user = User.new
+    p 'new'
   end
 
   def account
-   if session[:user]
-     @user = User.find_by(id: session[:user])
-   else
-     flash[:notice] = 'Please Sign In'
-     redirect_to '/login'
-   end
+    if session[:user]
+      @user = User.find_by(id: session[:user])
+    else
+      flash[:notice] = 'Please Sign In'
+      redirect_to '/login'
+    end
   end
 
 
